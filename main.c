7f8b2c922d2d034a322843ad81c418992c7dd571
Kitty <4lessandro@proton.me>
2026-06-19 21:27:58 +0000

    v0.2
    Lmao, this had a segmentation fault I discovered by myself xD

--- main.c
+++ main.c
@@ -5,13 +5,18 @@
 #define BUFFER_SIZE 1024
 
 int main(int argc, char *argv[]) {
-    // Guardar la terminal actual para luego ponerla de nuevo al cerrar
-    printf("\033[?1049h");
-
     FILE *file;
     char line[BUFFER_SIZE];
 
-    if (argc != 2) {
+    // --version
+    if(argc > 1 &&
+        (strcmp(argv[1], "--version") == 0 ||
+         strcmp(argv[1], "-v") == 0)){
+        printf("quecto text editor v0.02\nRepo: https://github.com/kittyteggie/quecto\n");
+        return 0;
+    }
+
+    if(argc != 2){
         printf("Usage: %s <filename>\n", argv[0]);
         return 1;
     }
@@ -20,7 +25,7 @@ int main(int argc, char *argv[]) {
     // TODO: Poder editar archivos existentes
     file = fopen(argv[1], "r");
 
-    if (file != NULL) {
+    if(file != NULL){
         fclose(file);
         printf("ERROR: This file already exists, we'll add a file edit option soon\n");
         return 1;
@@ -29,33 +34,37 @@ int main(int argc, char *argv[]) {
     // Crear archivo
     file = fopen(argv[1], "w");
 
-    if (file == NULL) {
+    if(file == NULL){
         printf("ERROR: Could not create file\n");
         return 1;
     }
 
-    // Comienza el editor :0
+    printf("\033[?1049h");
+
+    // Comienza el editor
     printf("\033[2J");
     printf("\033[H");
+
     printf("\033[30;47m");
-    printf(" quecto text editor v0.01 \n");
+    printf(" quecto text editor v0.02 \n");
     printf("\033[0m\n");
+
     printf("Type :w on an empty line to save and exit.\n\n");
 
-    while (1) {
-        if (fgets(line, sizeof(line), stdin) == NULL) {
-            break;
-        }
-      
-        // El :w para guardar el archivo
-        if (strcmp(line, ":w\n") == 0) {
+    while(1){
+        if(fgets(line, sizeof(line), stdin) == NULL){
             break;
         }
 
+	// El :w para guardar el archivo
+        if(strcmp(line, ":w\n") == 0){
+            break;
+        }
         fputs(line, file);
     }
-    printf("\033[?1049l");
 
     fclose(file);
+    printf("\033[?1049l");
+
     return 0;
 }
