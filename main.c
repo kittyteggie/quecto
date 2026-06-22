6d28ced0bcfe0d1f78ca5fa717bafa3d19cb8d82
Kitty <4lessandro@proton.me>
2026-06-22 22:41:15 +0000

    Some code cleaning

--- main.c
+++ main.c
@@ -13,21 +13,17 @@ int main(int argc, char *argv[]) {
     int line_count = 0;
     int edit_previous = 0;
 
-    // --version & -v
     if(argc > 1 &&
         (strcmp(argv[1], "--version") == 0 ||
         strcmp(argv[1], "-v") == 0)){
         printf("quecto text editor v0.04\nRepo: https://github.com/kittyteggie/quecto\n");
         return 0;
     }
-
     if(argc != 2){
         printf("Usage: %s <filename>\n", argv[0]);
         return 1;
     }
-
-    // Check if a file already exists
-    // TODO: Edit already existing files
+    // Check if a file already exists | TODO: Edit already existing files
     file = fopen(argv[1], "r");
 
     if(file != NULL){
@@ -35,7 +31,6 @@ int main(int argc, char *argv[]) {
         printf("ERROR: This file already exists, we'll add a file edit option soon\n");
         return 1;
     }
-
     // Make the file
     file = fopen(argv[1], "w");
 
@@ -60,26 +55,20 @@ int main(int argc, char *argv[]) {
         if(fgets(line, sizeof(line), stdin) == NULL){
             break;
         }
-
         // :w to save the file
         if(strcmp(line, ":w\n") == 0){
-
             for(int i = 0; i < line_count; i++){
                 fputs(lines[i], file);
             }
-
             break;
         }
-
-        // :q to quit without saving
-        // TODO: REMAKE IT WHEN ALREADY EXISTING FILES CAN BE READ
+        // :q to quit without saving | TODO: REMAKE IT WHEN ALREADY EXISTING FILES CAN BE READ
         if(strcmp(line, ":q\n") == 0){
             fclose(file);
             remove(argv[1]);
             printf("\033[?1049l");
             return 0;
         }
-
         // :f to go to the previous line
         if(strcmp(line, ":f\n") == 0){
             if(line_count > 0){
@@ -87,21 +76,17 @@ int main(int argc, char *argv[]) {
             }
             continue;
         }
-
         if(edit_previous){
             strcpy(lines[line_count - 1], line);
             edit_previous = 0;
             continue;
         }
-
         if(line_count < MAX_LINES){
             strcpy(lines[line_count], line);
             line_count++;
         }
     }
-
     fclose(file);
     printf("\033[?1049l");
-
-   return 0;
+    return 0;
 }
